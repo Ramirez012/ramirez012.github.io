42c3b4d3d2af5963912a50cd083485b1fcb763e1
Ramirez012 <nasyrov.ramir@mail.ru>
2021-10-28 19:23:09 +0600

    site

---
+++ js/main.js
@@ -0,0 +1,68 @@
+// Самовызывающаяся функция (код отрабатывается сразу при загрузке main.js)
+(function () {
+    const header = document.querySelector('.header');
+    // При скролле страница на 50 пикселей вниз меняется бэкграунд у фиксированного списка (добавляется другое класс)
+    // В противнос случае удаляем этот класс
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header_active');
+        } else {
+            header.classList.remove('header_active');
+        }
+    };
+}());
+
+// Burger Handler (вывод бургера при клике на иконку)
+
+(function() {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav_active');        
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
+    })
+}());
+
+
+// Прокрутка сайта по ссылкам (плавный скролл)
+(function() {
+    const smoothScroll = function (targetEl, duration) {
+        // Высота header (чтобы не было наездов) 
+        // Вставляем селектор header
+        const headerElHeight = document.querySelector('.header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+        // Как именно будет анимироваться скролл
+        const ease = function(t,b,c,d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+        // Функция анимации
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+    };
+    // Подвешивание обработчика события на сами ссылки
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}());
